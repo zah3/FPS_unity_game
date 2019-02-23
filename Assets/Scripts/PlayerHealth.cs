ba971312e6674d62f74754be74219eb2e6d15d58
Zachariasz <zah3@o2.pl>
2019-02-23 22:47:59 +0100

    repair red desktop

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,22 +4,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealth : MonoBehaviour {
-    
+public class PlayerHealth : MonoBehaviour
+{
     public float start_health = 100f;
     public float current_health = 100f;
     public Image health_bar;
-    public Image damageImage; 
-    public float flashSpeed = 5f;                             
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     
-   
-    
-	public void TakeDamage()
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    bool _damaged;
+
+    private void Update()
     {
+        if (_damaged)
+        {
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        _damaged = false;
+    }
+
+    public void TakeDamage()
+    {
+        _damaged = true;
         health_bar.fillAmount = current_health / start_health;
-        damageImage.color = flashColour;
-        damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-	}
+    }
 
     public void ApplyDamage(float damage)
     {
@@ -28,7 +41,7 @@ public class PlayerHealth : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+
         TakeDamage();
     }
-   
 }
